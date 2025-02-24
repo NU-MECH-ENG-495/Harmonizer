8e1ac5189dda7e0ea0607c290b174a16ea46d734
Joseph Blom <jblom5400@gmail.com>
2025-02-24 17:16:00 -0600

    working motor control, just need midi parse.

---
+++ sketch_steper/stepNotes.ino
@@ -0,0 +1,91 @@
+#include <TMC2209.h>
+
+// SoftwareSerial can be used on Arduino boards without HardwareSerial ports,
+// such as the Uno, Nano, and Mini.
+//
+// See this reference for more details:
+// https://www.arduino.cc/reference/en/language/functions/communication/serial/
+
+// Software serial ports should only be used for unidirectional communication
+// The RX pin does not need to be connected, but it must be specified when
+// creating an instance of a SoftwareSerial object
+const uint8_t RX_PIN = 7;
+const uint8_t TX_PIN = 6;
+SoftwareSerial soft_serial(RX_PIN, TX_PIN);
+
+const uint8_t STEP_PIN = 4;
+const uint8_t DIRECTION_PIN = 3;
+const uint32_t STEP_COUNT = 48000;
+const uint32_t STEP_PER_SCALE = STEP_COUNT / 12;
+const uint16_t HALF_STEP_DURATION_MICROSECONDS = 5;
+const uint16_t STOP_DURATION = 1000;
+// current values may need to be reduced to prevent overheating depending on
+// specific motor and power supply voltage
+const uint8_t RUN_CURRENT_PERCENT = 100;
+
+
+// Instantiate TMC2209
+TMC2209 stepper_driver;
+
+String inString = "";  // string to hold input
+
+int position = 0;
+int newPosition = 0;
+
+void setup()
+{
+  Serial.begin(9600);
+  Serial.println("<Arduino is ready>");
+
+  stepper_driver.setup(soft_serial);
+
+  pinMode(STEP_PIN, OUTPUT);
+  pinMode(DIRECTION_PIN, OUTPUT);
+
+  stepper_driver.setRunCurrent(RUN_CURRENT_PERCENT);
+  stepper_driver.enableCoolStep();
+  stepper_driver.enable();
+}
+
+void loop()
+{
+  recvWithEndMarker();
+  stepFromPosition(position, newPosition);
+  position = newPosition;
+}
+
+void stepFromPosition(int from, int to) {
+  int shift = 0;
+  if (to > from) {
+    shift = to - from;
+    digitalWrite(DIRECTION_PIN, 1);
+  } else {
+    shift = from - to;
+    digitalWrite(DIRECTION_PIN, 0);
+  }
+
+  for (uint32_t i=0; i<STEP_PER_SCALE*2*shift; ++i)
+  {
+    digitalWrite(STEP_PIN, !digitalRead(STEP_PIN));
+    delayMicroseconds(HALF_STEP_DURATION_MICROSECONDS);
+  }
+}
+
+void recvWithEndMarker() {
+    while (Serial.available()==0) {}
+    while (Serial.available() > 0) {
+      int inChar = Serial.read();
+      if (isDigit(inChar)) {
+        // convert the incoming byte to a char and add it to the string:
+        inString += (char)inChar;
+      }
+      // if you get a newline, print the string, then the string's value:
+      if (inChar == '\n') {
+        Serial.print("Going to step:");
+        Serial.println(inString.toInt());
+        newPosition = inString.toInt();
+        // clear the string for new input:
+        inString = "";
+      }
+  }
+}
--- src/serielTest.cpp
+++ src/serielTest.cpp
@@ -1,20 +1,51 @@
 #include <stdio.h>
 #include <unistd.h>
+#include <iostream>
 #include <libserial/SerialPort.h>
-#include <MidiFile.h>
+#include "MidiFile.h"
 
-int main()
-{
-    using namespace std;
-    int data[] = {10,5,13};  //Random data we want to send
-    FILE *file;
-    file = fopen("/dev/ttyUSB0","w");  //Opening device file
-    int i = 0;
-    for(i = 0 ; i < 3 ; i++)
-    {
-        fprintf(file,"%d",data[i]); //Writing to the file
-        fprintf(file,"%c",','); //To separate digits
-        sleep(1);
+// using namespace LibSerial;
+
+int main() {
+    smf::MidiFile midiFile;
+    midiFile.read("../taps-3.mid");
+    int numTracks = midiFile.getTrackCount();
+    int timeDivision = midiFile.getTicksPerQuarterNote();
+    std::cout << timeDivision << std::endl;
+
+    for (int track = 0; track < numTracks; track++) {
+        for (int event = 0; event < midiFile[track].size(); event++) {
+            // Process MIDI events
+            // midiFile[track][event].getCommand() returns the MIDI command
+            // midiFile[track][event].getP1() and midiFile[track][event].getP2() return the parameters
+        }
     }
-    fclose(file);
+//   SerialPort serial_port;
+//   std::string port_name = "/dev/ttyUSB0"; // Replace with your serial port
+//   int baud_rate = 9600; // Set the desired baud rate
+
+//   try {
+//     serial_port.Open(port_name);
+//   } catch (const OpenFailed &e) {
+//     std::cerr << "Error opening serial port: " << e.what() << std::endl;
+//     return 1;
+//   }
+
+//   serial_port.SetBaudRate(static_cast<BaudRate>(baud_rate));
+//   serial_port.SetCharacterSize(CharacterSize::CHAR_SIZE_8);
+//   serial_port.SetStopBits(StopBits::STOP_BITS_1);
+//   serial_port.SetParity(Parity::PARITY_NONE);
+
+//   std::string data_to_send = "3\n"; // rotate 90 degrees
+
+//   try {
+//     serial_port.Write(data_to_send);
+//     std::cout << "Sent: " << data_to_send << std::endl;
+//   } catch (const std::runtime_error &e) {
+//     std::cerr << "Error writing to serial port: " << e.what() << std::endl;
+//     return 1;
+//   }
+
+//   serial_port.Close();
+  return 0;
 }
